fe339342a075764830fbcb564ba441d605384a9e
Tristan <tristanpinzari@example.com>
2025-12-29 16:16:15 -0500

    Add project cards

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,6 +10,10 @@ import cloud9 from "./cloud9nobg.png";
 import resume1 from "./resumeparser1.gif";
 import resume2 from "./resumeparser2.gif";
 import resume3 from "./resumeparser3.png";
+import city1 from "./city1.png";
+import city2 from "./city2.png";
+import city3 from "./city3.png";
+import city4 from "./city4.png";
 
 export {
   party,
@@ -24,4 +28,8 @@ export {
   resume1,
   resume2,
   resume3,
+  city1,
+  city2,
+  city3,
+  city4,
 };
