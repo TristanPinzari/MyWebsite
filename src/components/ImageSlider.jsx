d618c09010a1a401180acddfa151e3d1ce42682b
Tristan <tristanpinzari@gmail.com>
2026-01-01 13:26:09 -0500

    Changed images of notify

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 
 function ImageSlider({ id, images = [] }) {
-  const [current, setCurrent] = useState(
-    images.length > 0 ? `${id}-slide-0` : null
-  );
-  const scrollIntoView = (e, slideId) => {
+  const [current, setCurrent] = useState(images.length > 0 ? 0 : null);
+
+  const scrollIntoView = (e, newValue) => {
     if (e) e.preventDefault();
-    const element = document.getElementById(slideId);
+    const element = document.getElementById(`${id}-slide-${newValue}`);
     if (element) {
-      setCurrent(slideId);
+      setCurrent(newValue);
       element.scrollIntoView({
         behavior: "smooth",
         block: "nearest",
@@ -17,20 +16,40 @@ function ImageSlider({ id, images = [] }) {
     }
   };
 
+  function handleScroll(e) {
+    const container = e.target;
+    if (container.scrollLeft == 0) {
+      setCurrent(0);
+    } else {
+      setCurrent(
+        Math.round(
+          container.scrollLeft / container.scrollWidth / (1 / images.length)
+        )
+      );
+    }
+  }
+
   return (
     <div id={id} className="ImageSlider">
-      <div>
+      <div onScroll={handleScroll}>
         {images.map((src, i) => (
-          <img key={i} id={`${id}-slide-${i}`} src={src} />
+          <img
+            key={i}
+            id={`${id}-slide-${i}`}
+            src={src}
+            onClick={(e) =>
+              scrollIntoView(e, current + 1 >= images.length ? 0 : current + 1)
+            }
+          />
         ))}
       </div>
 
       <div>
         {images.map((_, i) => (
           <a
-            className={current == `${id}-slide-${i}` ? "active" : ""}
+            className={current == i ? "active" : ""}
             key={i}
-            onClick={(e) => scrollIntoView(e, `${id}-slide-${i}`)}
+            onClick={(e) => scrollIntoView(e, i)}
           />
         ))}
       </div>
