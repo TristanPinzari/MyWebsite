2341abaa767bfca4d395678a5d40bbf237d2ca02
Tristan <tristanpinzari@gmail.com>
2026-06-06 21:03:49 -0400

    fix: cors

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,25 +4,17 @@ export default function ContactForm() {
   const [result, setResult] = useState("Ready to connect and chat anytime!");
 
   const handleSubmit = (e) => {
-    const form = e.target;
-    const data = new URLSearchParams(new FormData(form)).toString();
     e.preventDefault();
-    fetch(form.action, {
+    fetch(["https://api.web", "3forms.com/submit"].join(""), {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: data,
+      body: new FormData(e.target),
     })
       .then((r) => r.json())
       .then((r) => setResult(r.success ? "Message sent!" : r.message));
   };
 
   return (
-    <form
-      id="contactForm"
-      className="fadeIn"
-      action={["https://api.web", "3forms.com/submit"].join("")}
-      onSubmit={handleSubmit}
-    >
+    <form id="contactForm" className="fadeIn" onSubmit={handleSubmit}>
       <p className="centerText">{result}</p>
       <input type="hidden" name="access_key" value={import.meta.env.VITE_WEB3FORMS_KEY} />
       <input type="text" name="name" placeholder="NAME" required />
